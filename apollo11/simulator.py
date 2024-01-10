8fad50c4a59785d02ebf3227697b6df2e5ea5ddc
Cristian Andres Yela <50997813+ChristYela@users.noreply.github.com>
2024-01-10 16:34:12 -0500

    Update simulator.py

--- apollo11/simulator.py
+++ apollo11/simulator.py
@@ -1,3 +1,4 @@
+# apollo11/simulator.py
 from typing import Dict, List
 from datetime import datetime
 import logging
@@ -8,9 +9,10 @@ from apollo11.report_generator import ReportGenerator
 from apollo11.dashboard_generator import DashboardGenerator
 
 class Apolo11Simulator:
-    def __init__(self, simulation_interval: int, max_files: int):
+    def __init__(self, simulation_interval: int, max_files: int, project: str):
         self.simulation_interval = simulation_interval
         self.max_files = max_files
+        self.project = project
         self.data_generator = DataGenerator()
         self.file_manager = FileManager()
         self.report_generator = ReportGenerator()
@@ -18,12 +20,12 @@ class Apolo11Simulator:
 
     def run_simulation(self, num_cycles: int):
         for _ in range(num_cycles):
-            data = self.data_generator.generate_data(self.max_files)
+            data = self.data_generator.generate_data(self.max_files, self.project)
             self.file_manager.manage_files(data)
             self.report_generator.generate_reports(data)
             self.file_manager.move_processed_files()
             self.dashboard_generator.generate_dashboard(data)
-            logging.info("Cycle completed. Waiting for the next cycle...")
+            logging.info("Ciclo completado. Esperando el próximo ciclo...")
 
-            # Añadir lógica para esperar el intervalo de simulación.
+            # Agregar lógica para esperar el intervalo de simulación.
             time.sleep(self.simulation_interval)
