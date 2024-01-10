8bb321b545b4d2f4c56f1ca2bc97c3be0aafa3e7
Cristian Andres Yela <50997813+ChristYela@users.noreply.github.com>
2024-01-10 16:33:32 -0500

    Update report_generator.py

--- apollo11/report_generator.py
+++ apollo11/report_generator.py
@@ -1,3 +1,4 @@
+# apollo11/report_generator.py
 import os
 import logging
 from datetime import datetime
@@ -11,26 +12,26 @@ class ReportGenerator:
         with open(report_path, 'w') as report_file:
             report_file.write("Reporte de Estadísticas\n\n")
 
-            for mission, device_states in self.mission_device_states.items():
-                report_file.write(f"\nMisión: {mission}\n")
+            for project, device_states in self.project_device_states.items():
+                report_file.write(f"\nProyecto: {project}\n")
                 for state, count in device_states.items():
                     report_file.write(f"{state}: {count} eventos\n")
 
             report_file.write("\nGestión de Desconexiones\n")
-            for mission in self.mission_device_states.keys():
-                unknown_count = self.mission_device_states[mission]['unknown']
-                report_file.write(f"Misión {mission}: {unknown_count} desconexiones\n")
+            for project in self.project_device_states.keys():
+                unknown_count = self.project_device_states[project]['unknown']
+                report_file.write(f"Proyecto {project}: {unknown_count} desconexiones\n")
 
-            report_file.write("\nConsolidación de Misiones\n")
-            total_faulty_devices = sum(self.mission_device_states[mission]['faulty'] for mission in self.mission_device_states)
+            report_file.write("\nConsolidación de Proyectos\n")
+            total_faulty_devices = sum(self.project_device_states[project]['faulty'] for project in self.project_device_states)
             report_file.write(f"Total de dispositivos inoperables: {total_faulty_devices}\n")
 
             report_file.write("\nCálculo de Porcentajes\n")
-            for mission in self.mission_device_states.keys():
-                total_devices = sum(self.mission_device_states[mission][state] for state in self.DEVICE_STATES)
+            for project in self.project_device_states.keys():
+                total_devices = sum(self.project_device_states[project][state] for state in self.DEVICE_STATES)
                 for state in self.DEVICE_STATES:
-                    state_count = self.mission_device_states[mission][state]
+                    state_count = self.project_device_states[project][state]
                     percentage = (state_count / total_devices) * 100 if total_devices > 0 else 0
-                    report_file.write(f"{mission} - {state}: {percentage:.2f}%\n")
+                    report_file.write(f"{project} - {state}: {percentage:.2f}%\n")
 
         logging.info(f"Reporte generado: {report_filename}")
