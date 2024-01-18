ed09c03af257484220da9f98c6f2eb4cbaeb5afd
Cristian Andres Yela <50997813+ChristYela@users.noreply.github.com>
2024-01-18 16:26:16 -0500

    Update dashboard_generator.py
    comments added

--- apollo11/dashboard_generator.py
+++ apollo11/dashboard_generator.py
@@ -1,4 +1,5 @@
 # apollo11/dashboard_generator.py
+
 import os
 import logging
 import json
@@ -11,20 +12,25 @@ class DashboardGenerator:
         self.mission_device_states: Dict[str, Dict[str, int]] = {}
 
     def generate_dashboard(self, data: Dict[str, List[Dict[str, str]]]):
+        # Nombre y ruta del archivo de tablero de control
         dashboard_filename = 'dashboard.json'
         dashboard_path = os.path.join('reports', dashboard_filename)
+
+        # Estructura de datos del tablero de control
         dashboard_data = {
             'last_update': datetime.now().strftime('%d%m%y%H%M%S'),
             'missions': {}
         }
 
+        # Agregar información de dispositivos por misión al tablero de control
         for mission, device_states in self.mission_device_states.items():
             dashboard_data['missions'][mission] = {
                 'total_devices': sum(device_states.values())
             }
 
+        # Escribir el tablero de control en un archivo JSON
         with open(dashboard_path, 'w') as dashboard_file:
             dashboard_file.write(json.dumps(dashboard_data, indent=2))
 
+        # Registrar en el registro que el tablero de control ha sido actualizado
         logging.info(f"Tablero de control actualizado: {dashboard_filename}")
-
