83dcb18fa6b4ca54c57efb6f00282caeeceb46cf
Cristian Andres Yela <50997813+ChristYela@users.noreply.github.com>
2024-01-29 17:11:43 -0500

    Update test_report_generator.py
    Update test_report_generator.py with pytest

--- tests/test_report_generator.py
+++ tests/test_report_generator.py
@@ -1,20 +1,24 @@
-import unittest
-import os
+# tests/test_report_generator.py
+import pytest
+from io import StringIO
+import json
 from apollo11.report_generator import ReportGenerator
 
-class TestReportGenerator(unittest.TestCase):
-    def test_generate_reports(self):
-        # Prueba la función generate_reports en report_generator.py
-        report_generator = ReportGenerator()
-        data = {'ORBONE': [{'date': '210124120000', 'mission': 'ORBONE', 'device_type': 'device_1', 'device_status': 'excellent', 'hash': 'hash_value'}]}
-        report_generator.generate_reports(data)
-        self.assertTrue(os.path.exists('reports'))
+def test_generate_reports(tmp_path):
+    report_generator = ReportGenerator()
 
-    def test_get_current_timestamp(self):
-        # Prueba la función get_current_timestamp en report_generator.py
-        report_generator = ReportGenerator()
-        timestamp = report_generator.get_current_timestamp()
-        self.assertTrue(timestamp)
+    data = {'project1': [{'date': '210124120000', 'mission': 'ORBONE', 'device_type': 'device_1', 'device_status': 'excellent', 'hash': 'hash1'},
+                            {'date': '210124121500', 'mission': 'ORBONE', 'device_type': 'device_2', 'device_status': 'good', 'hash': 'hash2'}]}
 
-if __name__ == '__main__':
-    unittest.main()
+    report_generator.generate_reports(data)
+
+    report_filename = f'APLSTATS-REPORT-210124120000.log'
+    report_path = tmp_path / 'reports' / report_filename
+
+    assert report_path.is_file()
+
+    with open(report_path, 'r') as report_file:
+        report_content = json.load(report_file)
+        assert 'project1' in report_content
+        assert 'excellent' in report_content['project1']
+        assert report_content['project1']['excellent'] == 1
